1456264aefbc33b2837c2e536f095579cef4e136
SangRok Jung <sesilao@naver.com>
2023-03-27 19:07:10 +0900

    clear Grading_Students

---
+++ hackerrank/Grading_Students.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def round_a(x):
+    return result
+
+
+def gradingStudents(grades):
+    result = []
+    for grde in grades:
+        if grde < 38:
+            result.append(grde)
+        else:
+            next_n = 5
+            while next_n < grde:
+                next_n += 5
+
+            dif_score = next_n - grde
+            print(dif_score)
+
+            if dif_score < 3:
+                result.append(next_n)
+            else:
+                result.append(grde)
+
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
